e3e998dc8a17273bb9472400a2d7fb47820f0963
Anita <zorrodeplata.anita9@gmail.com>
2022-09-08 13:36:51 +0500

    Lesson 41

---
+++ Lesson_41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+RandomNumbers(arr);
+void RandomNumbers(int[] arr)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+    Console.WriteLine(string.Join(", ", arr));
+}
+int count = 0;
+for (int j = 0; j < arr.Length; j++)
+{
+    if (arr[j] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Колличество чисел больше 0: {count}");
